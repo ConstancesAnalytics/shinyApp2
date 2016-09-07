8f3afff9ee1d6ec5507f239e05683ef6bee2128a
elienonnon <elie.m.salem@gmail.com>
2016-09-07 14:36:42 +0200

    ajouter la variable par_ces dans la selection des variables

--- ajouter_var_correction.R
+++ ajouter_var_correction.R
@@ -12,7 +12,7 @@ para$age=as.numeric(((para$SOC_DatExam-para$SOC_DNaissance )/365.5))
 
 para$resp_proc <- 'OK'
 para$bras <- 'unknown'
-
+
 para$resp_proc <- ifelse(!is.na(para$TAR_TenArtSysDr) & !is.na(para$TAR_TenArtDiaDr) & (para$TAR_TenArtSysDr < para$TAR_TenArtDiaDr), 'notOK_test_inversion', para$resp_proc)
 para$resp_proc <- ifelse(!is.na(para$TAR_TenArtSysGa) & !is.na(para$TAR_TenArtDiaGa) & (para$TAR_TenArtSysGa < para$TAR_TenArtDiaGa), 'notOK_test_inversion', para$resp_proc)
 para$resp_proc <- ifelse(!is.na(para$TAR_TenArtSysBraRefDr) & !is.na(para$TAR_TenArtDiaBraRefDr) & (para$TAR_TenArtSysBraRefDr < para$TAR_TenArtDiaBraRefDr), 'notOK_ref_inversion', para$resp_proc)
@@ -62,7 +62,68 @@ for(i in 1:nrow(para)) {
   }
 }
 
+
+as.data.frame(table(para$resp_proc))
+
+# ----------------- add more variables
+
+#IMC
+para$HAU_MesTail2 <- as.numeric(para$HAU_MesTail)*0.01
+para$IMC <- (para$POI_MesPoi)/(para$HAU_MesTail2^2)
+para$class_IMC <- as.factor(cut(para$IMC, breaks = c(12,18.5,24.9,29.9,70), right = FALSE, include.lowest = TRUE))
+
+#class_BIO_ChoHDL
+para$class_BIO_ChoHDL <- cut(para$BIO_ChoHDL, breaks = c(0.25,0.9, 3.5), right = FALSE,include.lowest = TRUE)
+
+#class_BIO_Trig
+para$class_BIO_Trig <- cut(para$BIO_Trig, breaks = c(0.2,2.5,30), right = FALSE,include.lowest = TRUE)
+
+#class_BIO_ChoTot
+para$class_BIO_ChoTot <- cut(para$BIO_ChoTot, breaks = c(1.5,2.2,6.0,15), right = FALSE,include.lowest = TRUE)
+
+#LDL
+para$BIO_ChoTot_LDL <- borne(para$BIO_ChoTot, 1.5 ,15)
+para$BIO_ChoHDL_LDL <- borne(para$BIO_ChoHDL, 0.25 ,3.5)
+para$BIO_Trig_LDL   <- borne(para$BIO_Trig, 0.1 ,3.75)
+
+para$ldl <- as.numeric(para$BIO_ChoTot_LDL-para$BIO_ChoHDL_LDL-(para$BIO_Trig_LDL/2.2))
+para$class_ldl <- as.factor(cut(para$ldl, breaks = c(-1,4.2,11), right = FALSE,include.lowest = TRUE))
+
+#SEXe level
+levels(para$SOC_Sex) <- c('M','F')
+
+#Tour de taille
+
+para$RTH  <- round(para$TAI_MesToTai/para$HAN_MesToHan,2)
+
+para$class_rth  <- as.factor(ifelse( ((para$SOC_Sex==2 & para$RTH <=0.8 ) | (para$SOC_Sex==1 & para$RTH <=0.95 )),'Normal', 'Ob?sit? abdominale'))
+
+
+para$VEMS  <- apply(para[,c('SPI_VEMS1','SPI_VEMS2','SPI_VEMS3')],1, max, na.rm=FALSE)
+para$CVF  <- apply(para[,c('SPI_CVF1','SPI_CVF1','SPI_CVF1')],1, max, na.rm=FALSE)
+
+para$Insuf_resp  <- round(para$VEMS/para$CVF*100,2)
+para$Class_Insuf_resp  <- as.factor(cut(para$Insuf_resp, breaks = c(0,70,500),  right = FALSE,include.lowest = TRUE))
+
+para$VDL_AC_Mal  <-  ifelse(para$VDL_OeDrAvCorr > para$VDL_OeGaAvCorr ,para$VDL_OeDrAvCorr ,para$VDL_OeGaAvCorr)
+para$VDL_CLASS_AC_Mal  <- as.factor(ifelse(para$VDL_AC_Mal >3 ,'NORMAL','MALVOYANCE'))
+
+
+para$VDL_SC_Mal  <-  ifelse(para$VDL_OeDrSaCorr > para$VDL_OeGaSaCorr ,para$VDL_OeDrSaCorr ,para$VDL_OeGaSaCorr)
+para$VDL_CLASS_SC_Mal  <-  as.factor(ifelse(para$VDL_SC_Mal >3 ,'NORMAL','MALVOYANCE'))
+
+para$par_ces <- para$SOC_CES_NCes
+
+
+
+# manual modifications (a voir)
+para$SOC_NConstances <- as.integer(para$SOC_NConstances)
+para$SOC_Sex <- as.factor(para$SOC_Sex)
+para$SOC_CES_NCes <- as.factor(para$SOC_CES_NCes)
+para$SOC_anne <- as.factor(format(para$SOC_DatExam, "%Y"))
+para$SOC_moisanne <- as.factor(format(para$SOC_DatExam, "%m%y"))
+
 save(para, file = "paracl_romain.RData")
 
 
-as.data.frame(table(para$resp_proc))
+
--- global.R
+++ global.R
@@ -8,7 +8,8 @@ source('./function.R')
 # ----------------- load para file
 
 load('./paracl_romain.RData')
-colnames(para) <- gsub("PARACL_","", colnames(para))
+
+#colnames(para) <- gsub("PARACL_","", colnames(para))
 
 # ----------------- load dictionnary
 dic_nom_para <- read.csv('dic_nom_para.csv',header = TRUE, sep=';', encoding = "UTF-8")
@@ -131,6 +132,7 @@ bounds_para <- list(POI_MesPoi =c( 40,200 ),
 # ----------------- select variables
 
 para <- select(para, ### ELIE : select variables from dictionnary
+               par_ces,
                VDL_BiAvCorr,
                VDL_BiSaCorr,
                VDL_CeProAmbMono,
@@ -298,65 +300,7 @@ para$AUD_AuGa4000 <- as.numeric(as.character(para$AUD_AuGa4000))
 para$AUD_AuGa8000 <- as.numeric(as.character(para$AUD_AuGa8000))
 
 
-# ----------------- add more variables
 
-#IMC
-para$HAU_MesTail2=as.numeric(para$HAU_MesTail)*0.01
-para$IMC=(para$POI_MesPoi)/(para$HAU_MesTail2^2)
-para$class_IMC=as.factor(cut(para$IMC, breaks = c(12,18.5,24.9,29.9,70), right = FALSE, include.lowest = TRUE))
-
-#class_BIO_ChoHDL
-para$class_BIO_ChoHDL=cut(para$BIO_ChoHDL, breaks = c(0.25,0.9, 3.5), right = FALSE,include.lowest = TRUE)
-
-#class_BIO_Trig
-para$class_BIO_Trig=cut(para$BIO_Trig, breaks = c(0.2,2.5,30), right = FALSE,include.lowest = TRUE)
-
-#class_BIO_ChoTot
-para$class_BIO_ChoTot=cut(para$BIO_ChoTot, breaks = c(1.5,2.2,6.0,15), right = FALSE,include.lowest = TRUE)
-
-#LDL
-para$BIO_ChoTot_LDL = borne(para$BIO_ChoTot, 1.5 ,15)
-para$BIO_ChoHDL_LDL= borne(para$BIO_ChoHDL, 0.25 ,3.5)
-para$BIO_Trig_LDL  =borne(para$BIO_Trig, 0.1 ,3.75)
-
-para$ldl= as.numeric(para$BIO_ChoTot_LDL-para$BIO_ChoHDL_LDL-(para$BIO_Trig_LDL/2.2))
-para$class_ldl=as.factor(cut(para$ldl, breaks = c(-1,4.2,11), right = FALSE,include.lowest = TRUE))
-
-#SEXe level
-levels(para$SOC_Sex)<-c('M','F')
-
-#Tour de taille
-
-para$RTH=round(para$TAI_MesToTai/para$HAN_MesToHan,2)
-
-para$class_rth=as.factor(ifelse( ((para$SOC_Sex==2 & para$RTH <=0.8 ) | (para$SOC_Sex==1 & para$RTH <=0.95 )),'Normal', 'Ob?sit? abdominale'))
-
-
-para$VEMS =apply(para[,c('SPI_VEMS1','SPI_VEMS2','SPI_VEMS3')],1, max, na.rm=FALSE)
-para$CVF =apply(para[,c('SPI_CVF1','SPI_CVF1','SPI_CVF1')],1, max, na.rm=FALSE)
-
-para$Insuf_resp=round(para$VEMS/para$CVF*100,2)
-para$Class_Insuf_resp=as.factor(cut(para$Insuf_resp, breaks = c(0,70,500),  right = FALSE,include.lowest = TRUE))
-
-para$VDL_AC_Mal = ifelse(para$VDL_OeDrAvCorr > para$VDL_OeGaAvCorr ,para$VDL_OeDrAvCorr ,para$VDL_OeGaAvCorr)
-para$VDL_CLASS_AC_Mal= as.factor(ifelse(para$VDL_AC_Mal >3 ,'NORMAL','MALVOYANCE'))
-
-
-para$VDL_SC_Mal = ifelse(para$VDL_OeDrSaCorr > para$VDL_OeGaSaCorr ,para$VDL_OeDrSaCorr ,para$VDL_OeGaSaCorr)
-para$VDL_CLASS_SC_Mal= as.factor(ifelse(para$VDL_SC_Mal >3 ,'NORMAL','MALVOYANCE'))
-
-para$par_ces <- para$SOC_CES_NCes
-
-
-
-# manual modifications (a voir)
-para$SOC_NConstances <- as.integer(para$SOC_NConstances)
-para$SOC_Sex <- as.factor(para$SOC_Sex)
-para$SOC_CES_NCes <- as.factor(para$SOC_CES_NCes)
-para$SOC_DNaissance <- as.Date(para$SOC_DNaissance, "%Y-%m-%d")
-para$SOC_DatExam <- as.Date(para$SOC_DatExam, "%Y-%m-%d")
-para$SOC_anne <- as.factor(format(para$SOC_DatExam, "%Y"))
-para$SOC_moisanne <- as.factor(format(para$SOC_DatExam, "%m%y"))
 
 
 # ----------------- Calcul de l'age
@@ -385,19 +329,19 @@ para_num$CESantenne <- para$SOC_CES_Antenne
 
 # ----------------- create clean para table (by excluding outliers)
 
-para_bounds <- para
-for (col in names(bounds_para)) {
-    vect <- bounds_para[[col]]
-    if(length(vect) == 2) {
-        if(identical(vect, c(1,2))) {
-            para_bounds[[col]][!(para_bounds[[col]] %in% vect)] <- NA
-        }
-        else {
-            para_bounds[[col]][(para_bounds[[col]] < vect[1]) | (para_bounds[[col]] > vect[2])] <- NA
-        }
-    }
-    else {
-        para_bounds[[col]][!(para_bounds[[col]] %in% vect)] <- NA
-    }
-}
+#para_bounds <- para
+#for (col in names(bounds_para)) {
+#    vect <- bounds_para[[col]]
+#    if(length(vect) == 2) {
+#        if(identical(vect, c(1,2))) {
+#            para_bounds[[col]][!(para_bounds[[col]] %in% vect)] <- NA
+#        }
+#        else {
+#            para_bounds[[col]][(para_bounds[[col]] < vect[1]) | (para_bounds[[col]] > vect[2])] <- NA
+#        }
+#    }
+#    else {
+#        para_bounds[[col]][!(para_bounds[[col]] %in% vect)] <- NA
+#    }
+#}
 
